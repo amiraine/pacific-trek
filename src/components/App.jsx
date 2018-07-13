a6b72f42797602a917e7883696936bcd4ed2c964
Ami <ik4rus.info@gmail.com>
2018-07-13 16:10:58 -0700

    instantiate components, begin to write functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Navigation from './Navigation';
+import Browse from './Browse';
+import Home from './Home';
+import Login from './Login';
+import Search from './Search';
+import Profile from './Profile';
 
 function App(){
 
@@ -8,11 +13,11 @@ function App(){
     <div>
       <Navigation/>
       <Switch>
-        <Route exact path="/" component={}/>
-        <Route path="/browse" component={}/>
-        <Route path="/search" component={}/>
-        <Route path="/login" component={}/>
-        <Route path="/profile" component={}/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/browse" component={Browse}/>
+        <Route path="/search" component={Search}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/profile" component={Profile}/>
       </Switch>
     </div>
   );
