9e40f5cc78e749164cefebf95431cb3ac3ccae54
Ami Cooper <amiraine.cooper@gmail.com>
2018-07-18 17:27:50 -0700

    day end

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import family from '../../src/assets/images/family-icon.png';
+import points from '../../src/assets/images/points.png';
+
 import { Link } from 'react-router-dom'
 
 function Browse(){
   return(
-    <div>
-      
+    <div className='wrapper browse-wrapper'>
+      <style jsx>{`
+        .browse-wrapper{
+          display: flex;
+          height: 70vh;
+          padding: 75px;
+          font-family: 'Alegreya Sans SC', sans-serif;
+          flex-wrap: wrap;
+        }
+        .browse-icon-group img{
+          max-height: 215px;
+        }
+        .browse-icon-group{
+          text-align: center;
+          margin: 25px;
+        }
+      `}</style>
+      <div className='browse-icon-group'>
+        <img src={family}/>
+        <h1>Family-friendly</h1>
+      </div>
+      <div className='browse-icon-group'>
+        <img src={points}/>
+        <h1>Viewpoints</h1>
+      </div>
     </div>
   )
 }
