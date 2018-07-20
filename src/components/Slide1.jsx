2ad110f240d8b1ae65ebb3d557b697ffee703ddd
Student <example@example.com>
2018-07-20 15:04:19 -0700

    dry up css for slides, add fade animation

--- src/components/Slide1.jsx
+++ src/components/Slide1.jsx
@@ -2,23 +2,10 @@ import React from 'react'
 
 function Slide1(){
   return(
-    <div className='slide'>
+    <div className='slide one'>
       <style jsx>{`
-        .slide{
-          width: 100%;
+        .one{
           background-image: url('https://traveloregon.com/wp-content/uploads/2015/06/CraterLake.jpg');
-          max-height: 1500px;
-          min-height: 50vh;
-          background-position: center bottom;
-          background-attachment: fixed;
-          background-size: cover;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          color: white;
-          text-align: center;
-          font-size: 4em;
-          font-family: 'Alegreya Sans SC', sans-serif;
         }
       `}</style>
       <h1>Explore the Pacific Northwest</h1>
